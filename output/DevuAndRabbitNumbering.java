3872591fe1b3bd3f6289bcdeccf3382b3b67a58f
emli <emilbeksulaymanov@gmail.com>
2021-10-08 18:59:46 +0600

    Fixed solutions after contest

--- output/DevuAndRabbitNumbering.java
+++ output/DevuAndRabbitNumbering.java
@@ -5,27 +5,15 @@ public class DevuAndRabbitNumbering {
     public String canRenumber(int[] rabbitIds) {
         Arrays.sort(rabbitIds);
 
-        int left = 0;
-        int right = rabbitIds.length - 1;
-
         HashSet<Integer> set = new HashSet<>();
-        while (left < right) {
-            if (!set.contains(rabbitIds[left] - 1)) {
-                set.add(rabbitIds[left] - 1);
-                left++;
-            } else if (!set.contains(rabbitIds[left])) {
-                set.add(rabbitIds[left]);
-                left++;
-            } else {
-                return "cannot";
-            }
 
-            if (!set.contains(rabbitIds[right] + 1)) {
-                set.add(rabbitIds[right] + 1);
-                right--;
-            } else if (!set.contains(rabbitIds[right])) {
-                set.add(rabbitIds[right]);
-                right--;
+        for (int rabbitId : rabbitIds) {
+            if (!set.contains(rabbitId - 1)) {
+                set.add(rabbitId - 1);
+            } else if (!set.contains(rabbitId)) {
+                set.add(rabbitId);
+            } else if (!set.contains(rabbitId + 1)) {
+                set.add(rabbitId + 1);
             } else {
                 return "cannot";
             }
